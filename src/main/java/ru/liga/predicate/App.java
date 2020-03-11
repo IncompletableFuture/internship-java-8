ef157f3565ca1833930b97dfb6f3679855813135
boris-dev <boris.shestakov@gmail.com>
2020-03-11 19:47:17 +0300

    live coding

--- src/main/java/ru/liga/predicate/App.java
+++ src/main/java/ru/liga/predicate/App.java
@@ -1,6 +1,16 @@
 package ru.liga.predicate;
 
+import ru.liga.predicate.domain.Person;
 import ru.liga.predicate.repo.PersonRepository;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * Created by dzharkov on 11.03.2020.
@@ -11,6 +21,43 @@ public class App {
     private static PersonRepository repository = new PersonRepository();
 
     public static void main(String[] args) {
+        Function<Boolean, String> translate = (flag) -> flag ? "Да" : "Нет";
 
+        Consumer<Person> printConsumer = (person) -> {
+            String result = "Person{" +
+                    "fio='" + person.getFio() + '\'' +
+                    ", dateOfBirth=" + person.getDateOfBirth() +
+                    ", gender=" + person.getGender() +
+                    ", isMarried=" + translate.apply(person.isMarried())+
+                    ", isStudent=" + translate.apply(person.isStudent()) +
+                    ", hasJob=" + translate.apply(person.isHasJob() )+
+                    '}';
+
+            System.out.println(result);
+        };
+
+        List<Person> personList = repository.fetchAllPersons();
+
+        Predicate<Person> isAdultAndNotMarried = (person) -> ChronoUnit.YEARS.between(person.getDateOfBirth(), LocalDate.now()) >= 18;
+
+        Predicate<Person> isNotMarried = (person) -> !person.isMarried();
+
+        Predicate<Person> commonPredicate = isAdultAndNotMarried.and(isNotMarried);
+
+        List<Person> filteredPerson = filter(personList, commonPredicate);
+
+        filteredPerson.forEach(printConsumer);
+    }
+
+    private static List<Person> filter(List<Person> personList, Predicate<Person> condition) {
+        List<Person> result = new ArrayList<>();
+
+        for (Person person : personList) {
+            if (condition.test(person)) {
+                result.add(person);
+            }
+        }
+
+        return result;
     }
 }
