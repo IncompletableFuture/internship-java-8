924a8b002fa739ad291dc34d44da9d3ab5031bf9
dzharkov <dzharkov@phoenixit.ru>
2020-03-11 18:46:16 +0300

    Добавил шаблон для лайв-кодинга.

---
+++ src/main/java/ru/liga/predicate/App.java
@@ -0,0 +1,16 @@
+package ru.liga.predicate;
+
+import ru.liga.predicate.repo.PersonRepository;
+
+/**
+ * Created by dzharkov on 11.03.2020.
+ */
+
+public class App {
+
+    private static PersonRepository repository = new PersonRepository();
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ru/liga/predicate/domain/Gender.java
@@ -0,0 +1,9 @@
+package ru.liga.predicate.domain;
+
+/**
+ * Created by dzharkov on 11.03.2020.
+ */
+
+public enum Gender {
+    FEMALE, MALE
+}
