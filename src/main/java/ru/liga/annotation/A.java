49683fb6f48069250b1a38c8e71fcefa86727d5b
dzharkov <dzharkov@phoenixit.ru>
2020-03-13 18:35:02 +0300

    Добавил шаблон для аннотаций.

---
+++ src/main/java/ru/liga/annotation/A.java
@@ -0,0 +1,13 @@
+package ru.liga.annotation;
+
+/**
+ * Created by dzharkov on 13.03.2020.
+ */
+
+public class A {
+    private String value;
+
+    public A(String value) {
+        this.value = value;
+    }
+}
--- src/main/java/ru/liga/annotation/Main.java
+++ src/main/java/ru/liga/annotation/Main.java
@@ -6,6 +6,10 @@ package ru.liga.annotation;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(readSecretField(new A("123")));
+    }
 
+    private static String readSecretField(Object o) {
+        return null;
     }
 }
