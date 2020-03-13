576bec1d4ae7a7be24be7c817f141d028ca556b2
dzharkov <dzharkov@phoenixit.ru>
2020-03-13 17:19:51 +0300

    Добавил пример на interface segregation principle.

---
+++ src/main/java/ru/liga/refactoring/i/ICloud.java
@@ -0,0 +1,38 @@
+package ru.liga.refactoring.i;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
+
+public class ICloud implements CloudProvider {
+    @Override
+    public void storeFile(String fileName, byte[] file) {
+        System.out.println("Сохраняю файл" + fileName);
+    }
+
+    @Override
+    public byte[] getFile(String fileName) {
+        System.out.println("Возвращаю файл" + fileName);
+        return new byte[1024];
+    }
+
+    @Override
+    public void deleteFile(String fileName) {
+        System.out.println("Удаляю файл" + fileName);
+    }
+
+    @Override
+    public void createServer(String region) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public List<String> getServers(String region) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public boolean changeServerSpec(String serverId, List<String> spec) {
+        throw new NotImplementedException();
+    }
+}
